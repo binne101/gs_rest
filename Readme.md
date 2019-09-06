6d79bf92d3e00c2013b79f8613fe1b6eb944676f
binne101 <binne101@gmail.com>
2019-09-06 16:01:54 +0200

    PDP-7: GreetingController.java werkend maken. De feature manager is geinject.

--- Readme.md
+++ Readme.md
@@ -1,14 +1,17 @@
 Doing:
 
 TODO:
-- PDP-7: GreetingController.java werkend maken. De feature manager is geinject. 
-De feature is aanwezig in debug mode (debug werkt alleen via intellij mvnDebug werkt niet via remote).
-- PDP-7: Implementatie/bekijken van web-ui
+
+- PDP-7: Implementatie/bekijken van web-ui.
 - PDP-7: Het runtime aan/uitzetten van features.
 - PDP-7: Wat zijn alle opties?  (website doorlezen)
 - PDP-7: Optie van mail op gezette tijden tot de feature weg is. 
+- PDP-7: Strategy provider (b.v. release datum, serverip, clientip, gradualactivation, etc)
+- PDP-?: Swagger
+- PDP-?: mvnDebug werkt niet
                                                    
 Done:
+- PDP-7: GreetingController.java werkend maken. De feature manager is geinject. 
 - PDP-7: Feature toggle aan readme.md toevoegen
 
  
--- hateoas/src/main/java/nl/pdptst/hateoas/hello/GreetingController.java
+++ hateoas/src/main/java/nl/pdptst/hateoas/hello/GreetingController.java
@@ -1,10 +1,13 @@
 package nl.pdptst.hateoas.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.togglz.core.Feature;
+import org.togglz.core.manager.FeatureManager;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -15,16 +18,36 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @Value("${toggle.feature.enabled}")
-    private boolean toggleEnabled;
+    @Autowired
+    private FeatureManager manager;
+
+    public GreetingController(FeatureManager manager) {
+        this.manager = manager;
+    }
+
+    @Value("${switch.feature.enabled}")
+    private boolean switchEnabled;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        if (toggleEnabled){
-            name = "toggle";
+        if (switchEnabled){
+            name = "switch ";
         }
-
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+
+    @RequestMapping("/toggle")
+    public Greeting togglz(@RequestParam(value="name", defaultValue = "toggle_IT ") String name) {
+        StringBuilder nameBuilder = new StringBuilder(name);
+        for (Feature feature :manager.getFeatures()){
+            if (manager.isActive(feature)){
+                nameBuilder.append(feature.name()).append(" ");
+            }
+        }
+        name = nameBuilder.toString();
+        return new Greeting(101L, "toggle it: " + name);
+    }
+
 }
