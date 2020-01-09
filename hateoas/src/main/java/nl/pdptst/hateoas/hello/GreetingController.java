424966b62c16c6b45d06c11ec16562be9fed0492
binne <b>
2020-01-09 12:57:10 +0100

    De togglz gerelateerde code is verwijderd.

--- hateoas/src/main/java/nl/pdptst/hateoas/hello/GreetingController.java
+++ hateoas/src/main/java/nl/pdptst/hateoas/hello/GreetingController.java
@@ -1,13 +1,10 @@
 package nl.pdptst.hateoas.hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.togglz.core.Feature;
-import org.togglz.core.manager.FeatureManager;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,13 +14,6 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
-    @Autowired
-    private FeatureManager manager;
-
-    public GreetingController(FeatureManager manager) {
-        this.manager = manager;
-    }
 
     @Value("${switch.feature.enabled}")
     private boolean switchEnabled;
@@ -36,18 +26,4 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
-
-
-    @RequestMapping("/toggle")
-    public Greeting togglz(@RequestParam(value="name", defaultValue = "toggle_IT ") String name) {
-        StringBuilder nameBuilder = new StringBuilder(name);
-        for (Feature feature :manager.getFeatures()){
-            if (manager.isActive(feature)){
-                nameBuilder.append(feature.name()).append(" ");
-            }
-        }
-        name = nameBuilder.toString();
-        return new Greeting(101L, "toggle it: " + name);
-    }
-
 }
