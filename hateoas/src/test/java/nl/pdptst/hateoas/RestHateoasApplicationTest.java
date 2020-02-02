675cb490093b2952f5a80a6e35a69cdb3d0faef9
binne <b>
2020-02-02 17:45:25 +0100

    Demo gs_rest: add unit test, remove togglz dependencies

--- hateoas/src/test/java/nl/pdptst/hateoas/RestHateoasApplicationTest.java
+++ hateoas/src/test/java/nl/pdptst/hateoas/RestHateoasApplicationTest.java
@@ -2,11 +2,10 @@ package nl.pdptst.hateoas;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class RestHateoasApplicationTest {
 
     @Test
     public void main() {
+        assert true;
     }
 }
