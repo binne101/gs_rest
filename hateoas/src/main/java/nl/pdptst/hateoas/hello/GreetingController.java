50c5479230b7a22de0c01c5647311b6d37faa61c
binne <b>
2020-02-02 16:18:56 +0100

    Demo gs_rest: Enable togglz framework, works for local deployment.

--- hateoas/src/main/java/nl/pdptst/hateoas/hello/GreetingController.java
+++ hateoas/src/main/java/nl/pdptst/hateoas/hello/GreetingController.java
@@ -1,10 +1,13 @@
 package nl.pdptst.hateoas.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.togglz.core.Feature;
+import org.togglz.core.manager.FeatureManager;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -14,6 +17,13 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private FeatureManager manager;
+
+    public GreetingController(FeatureManager manager) {
+        this.manager = manager;
+    }
 
     @Value("${switch.feature.enabled}")
     private boolean switchEnabled;
@@ -26,4 +36,20 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+
+    @RequestMapping("/toggle")
+    public Greeting togglz(@RequestParam(value="name", defaultValue = "toggle_IT ") String name) {
+        StringBuilder nameBuilder = new StringBuilder("name=" + name);
+        for (Feature feature :manager.getFeatures()){
+            if (manager.isActive(feature)){
+                nameBuilder.append(" active feature=").append(feature.name());
+            } else {
+                nameBuilder.append(" not active feature=").append(feature.name());
+            }
+        }
+        name = nameBuilder.toString();
+        return new Greeting(101L, "toggle it: " + name);
+    }
+
 }
